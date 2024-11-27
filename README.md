1499adc78733766cd3aceeb70442eb509c1628b7
Muhammad Fakhar-e-Iqbal Abdullah <fakhareiqbal3534@gmail.com>
2024-11-27 20:29:48 +0500

    eval

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Chest-Cancer-with-MLFlow-DVC
+
+set MLFLOW_TRACKING_URI=https://dagshub.com/fakhar-iqbal/Chest-Cancer-with-MLFlow-DVC.mlflow
+
+set MLFLOW_TRACKING_USERNAME=fakhar-iqbal 
+
+set MLFLOW_TRACKING_PASSWORD=41054047873ee5f23a9675bfb86f858583143952
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from src.cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
+from src.cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier import logger
@@ -27,3 +29,28 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Training"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = EvaluationPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
